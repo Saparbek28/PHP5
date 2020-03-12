976ace181d30593e6e409b0699de1cdc2d5c5265
Saparbek28 <46023456+Saparbek28@users.noreply.github.com>
2020-03-12 14:56:23 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -6,66 +6,79 @@
  * Time: 4:27
  */
 
-$filename = 'filename.txt';
-$person = "John Smith\n";
 
 
-if (is_writable($filename)) {
+function CreateFilename(){
+    $filename = 'filename.txt';
+    $person = "John Smith\n";
+    if (is_writable($filename)) {
 
 
-    if (!$handle = fopen($filename, 'a')) {
-        echo "Не могу открыть файл ($filename)";
-        exit;
+        if (!$handle = fopen($filename, 'a')) {
+            echo "Не могу открыть файл ($filename)";
+            exit;
+        }
+
+        if (fwrite($handle, $person) === FALSE) {
+            echo "Не могу произвести запись в файл ($filename)";
+            exit;
+        }
+        echo 'OK';
+
+        file_put_contents($filename, $person, FILE_APPEND | LOCK_EX);
+
+
     }
 
-    if (fwrite($handle, $person) === FALSE) {
-        echo "Не могу произвести запись в файл ($filename)";
-        exit;
+
+    else {
+        echo "Файл $filename недоступен для записи";
     }
-    echo 'OK';
-    file_put_contents($filename, $person, FILE_APPEND | LOCK_EX);
 
 
-} else {
-    echo "Файл $filename недоступен для записи";
 }
 
 
-$books = array(
-    array(
+function ArrayFile(){
+    $books = array(
         array(
-            'author' => 'William Golding',
-            'title' => 'Lord of the Flies',
-            'year' => 1954
+            array(
+                'author' => 'William Golding',
+                'title' => 'Lord of the Flies',
+                'year' => 1954
+            ),
+            array(
+                'author' => 'R.D. Blackmore',
+                'title' => 'Lorna Doone',
+                'year' => 1869
+            )
         ),
         array(
-            'author' => 'R.D. Blackmore',
-            'title' => 'Lorna Doone',
-            'year' => 1869
+            array(
+                'author' => 'T.E. Lawrence',
+                'title' => 'Seven Pillars of Wisdom',
+                'year' => 1992
+            ),
+            array(
+                'author' => 'Truman Capote',
+                'title' => 'In Cold Blood',
+                'year' => 1966
+            )
         )
-    ),
-    array(
-        array(
-            'author' => 'T.E. Lawrence',
-            'title' => 'Seven Pillars of Wisdom',
-            'year' => 1992
-        ),
-        array(
-            'author' => 'Truman Capote',
-            'title' => 'In Cold Blood',
-            'year' => 1966
-        )
-    )
-);
+    );
 
 
 
-$filename = 'array.txt';
-$data = serialize($books);
+    
+
+    $filename = 'array.txt';
+    $data = serialize($books);
 //$data = json_encode($books);
-file_put_contents($filename,$data, FILE_APPEND | LOCK_EX);
+    file_put_contents($filename,$data, FILE_APPEND | LOCK_EX);
 
 
-$data = file_get_contents($filename);
+    $data = file_get_contents($filename);
 //$books = json_decode($data, TRUE);
-$books = unserialize($data);
+    $books = unserialize($data);
+
+}
